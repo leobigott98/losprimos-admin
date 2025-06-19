6fd3a0a41267973f22b256bde439a15e31609b59
leobigott98 <leonardo.bigott@correo.unimet.edu.ve>
2025-06-19 18:26:36 -0400

    changed acme image for testing purposes

--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -2,17 +2,24 @@ import Link from 'next/link';
 import NavLinks from '@/app/ui/dashboard/nav-links';
 import AcmeLogo from '@/app/ui/acme-logo';
 import { PowerIcon } from '@heroicons/react/24/outline';
+import Image from 'next/image';
 
 export default function SideNav() {
   return (
     <div className="flex h-full flex-col px-3 py-4 md:px-2">
       <Link
-        className="mb-2 flex h-20 items-end justify-start rounded-md bg-blue-600 p-4 md:h-40"
-        href="/"
+        className="mb-2 rounded-md bg-[url('/logo1.jpg')] bg-cover md:h-40"
+        href="/dashboard"
       >
-        <div className="w-32 text-white md:w-40">
+        {/* <Image 
+        alt='logo arepazo'
+        src='/logo1.jpg'
+        width={500}
+        height={500}
+      /> */}
+        {/* <div className="w-32 text-white md:w-40">
           <AcmeLogo />
-        </div>
+        </div> */}
       </Link>
       <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
         <NavLinks />
