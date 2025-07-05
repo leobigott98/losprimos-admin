6c34883758e73b49d2b29306cd77d43121676e08
leobigott98 <leonardo.bigott@correo.unimet.edu.ve>
2025-07-05 19:52:17 -0400

    fixed cloudsql connection

--- app/config/mysql.ts
+++ app/config/mysql.ts
@@ -1,10 +1,9 @@
 import mysql from 'mysql2/promise'
 
 export const pool  = mysql.createPool({
-  host: process.env.MYSQL_HOST,
-  port: Number(process.env.MYSQL_PORT),
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DB,
+  socketPath: process.env.INSTANCE_UNIX_SOCKET,
 });
 
