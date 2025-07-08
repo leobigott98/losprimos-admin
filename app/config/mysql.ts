adf09ea15003081716e69c513f4c3197c92c75b7
leobigott98 <leonardo.bigott@correo.unimet.edu.ve>
2025-07-08 16:47:28 -0400

    Changed mysql connection

--- app/config/mysql.ts
+++ app/config/mysql.ts
@@ -1,9 +1,22 @@
 import mysql from 'mysql2/promise'
+import { Connector } from '@google-cloud/cloud-sql-connector';
 
-export const pool  = mysql.createPool({
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DB,
-  socketPath: process.env.INSTANCE_UNIX_SOCKET
-});
+const instaceConnectionName = process.env.INSTANCE_CONNECTION_NAME || ''
+
+// connectWithConnector initializes a connection pool for a Cloud SQL instance
+// of MySQL using the Cloud SQL Node.js Connector.
+export const pool = async () => {
+  const connector = new Connector();
+  const clientOpts = await connector.getOptions({
+    instanceConnectionName: instaceConnectionName,
+  });
+  const dbConfig = {
+    ...clientOpts,
+    user: process.env.MYSQL_USER, // e.g. 'my-db-user'
+    password: process.env.MYSQL_PASSWORD, // e.g. 'my-db-password'
+    database: process.env.MYSQL_DB, // e.g. 'my-database'
+  };
+  // Establish a connection to the database.
+  return mysql.createPool(dbConfig);
+};
 
