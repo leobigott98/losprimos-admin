1729c39e1da1d0250de3b2c6f52b8794c9bb47a4
leobigott98 <leonardo.bigott@correo.unimet.edu.ve>
2025-07-09 01:10:26 -0400

    went back to original mysql connection configuration

--- app/config/mysql.ts
+++ app/config/mysql.ts
@@ -1,15 +1,9 @@
 import mysql from 'mysql2/promise'
-import { Connector, IpAddressTypes } from '@google-cloud/cloud-sql-connector';
 
-const connector = new Connector();
-const clientOpts = await connector.getOptions({
-  instanceConnectionName: process.env.INSTANCE_CONNECTION_NAME as string,
-  ipType: "PUBLIC" as IpAddressTypes,
-});
 export const pool = mysql.createPool({
-  ...clientOpts,
+  host: process.env.MYSQL_HOST,
+  port: process.env.MYSQL_PORT,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DB,
+  database: process.env.MYSQL_DB
 });
-
