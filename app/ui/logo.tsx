696c52a8488f47196943ea3aa0be697f28a596b0
leobigott98 <leonardo.bigott@correo.unimet.edu.ve>
2025-07-10 13:51:43 -0400

    logo image bug

--- app/ui/logo.tsx
+++ app/ui/logo.tsx
@@ -1,20 +1,23 @@
-import Image from 'next/image';
+import Image, { StaticImageData } from 'next/image';
 import Link from 'next/link';
+import LogoImage from '../../public/logo1.jpg';
+import HorizontalLogoImage from '../../public/logo1-horizontal.jpg'
+
 
 interface LogoProps {
   href?: string;
   alt?: string;
   className?: string;
-  horizontal?: string;
-  vertical?: string;
+  horizontal?: StaticImageData;
+  vertical?: StaticImageData;
 }
 
 export default function Logo({
   href = '/dashboard',
   alt = 'Logo',
   className = 'relative w-full overflow-hidden h-28 mb-2 rounded-md bg-cover md:h-40',
-  horizontal = '/logo1-horizontal.jpg',
-  vertical = '/logo1.jpg'
+  horizontal = HorizontalLogoImage,
+  vertical = LogoImage
 }: LogoProps) {
 
   return (
@@ -27,16 +30,12 @@ export default function Logo({
           src={horizontal}
           alt={alt}
           className="object-contain md:hidden"
-          width={500}
-          height={500}
         />
         {/* Desktop (vertical) */}
         <Image
           src={vertical}
           alt={alt}
           className="hidden object-contain md:block"
-          width={500}
-          height={500}
         />
       </div>
     </Link>
