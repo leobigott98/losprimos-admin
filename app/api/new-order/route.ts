a25fe59d3fcb792568bf1bff213d5371957141d2
leobigott98 <leonardo.bigott@correo.unimet.edu.ve>
2025-07-15 15:12:26 -0400

    added new-order route with post request to revalidate dashboard

---
+++ app/api/new-order/route.ts
@@ -0,0 +1,12 @@
+import { revalidatePath } from 'next/cache';
+
+export async function POST(request: Request){
+    revalidatePath('/dashboard/invoices');
+    revalidatePath('/dashboard')
+
+    return new Response(JSON.stringify({ message: 'Pages revalidated successfully!' }), {
+        status: 201,
+        headers: { 'Content-Type': 'application/json' },
+    });
+
+}
